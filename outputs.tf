395525f6d8416fefbc15b40eb1eb1075482438cd
Jason (Yue) Zhang <zhangyue1208@gmail.com>
2026-03-05 08:58:00 -0800

    refactor: integration cleanup infra-terraform-eks 06

--- outputs.tf
+++ outputs.tf
@@ -54,5 +54,5 @@ output "oidc_provider_arn" {
 # kubectl configuration command
 output "configure_kubectl" {
   description = "Command to configure kubectl"
-  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}-blue"
 }
